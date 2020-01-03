8072b059d6d158f3e70f6bc96fb56498582097b0
Mark Channer <markchanner@gmail.com>
2020-01-03 20:07:40 +0000

    Add UserList components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom';
 
 import Users from './user/containers/Users';
+import NewPlace from './places/containers/NewPlace';
 
 function App() {
   return (
     <Router>
-      <Route path="/">
-        <Users />
-      </Route>
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
     </Router>
   );
 }
--- src/user/containers/Users.js
+++ src/user/containers/Users.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
+import UsersList from '../components/UsersList';
+
 const Users = () => {
-  return <h2>Users Component</h2>;
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'Mark Channer',
+      image:
+        'https://assets.pernod-ricard.com/nz/media_images/test.jpg?hUV74FvXQrWUBk1P2.fBvzoBUmjZ1wct',
+      places: 3
+    }
+  ];
+
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
